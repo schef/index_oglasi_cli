4b6a42505d23c009a25c1bb299d5492bb94e811c
schef <zvanstefan@gmail.com>
2020-09-02 08:27:34 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # index_oglasi_cli
 Interact with Index Oglasi from python for better filtering.
+
+## dependencies
+
+`sudo pip install selenium`
+
+`sudo pacman -S geckodriver`
+
+## usage
+
+TODO
+
+## for more info look at
+
+[https://selenium-python.readthedocs.io](https://selenium-python.readthedocs.io)
---
+++ index_oglasi_cli.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
+
+if __name__ == "__main__":
+    pass
